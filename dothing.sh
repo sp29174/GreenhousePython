93854fda9ec85e3a5abca7740a5e0ba4e9381031
yeetmanthecoolman <75697942+yeetmanthecoolman@users.noreply.github.com>
2025-12-09 15:35:22 -0600

    Update dothing.sh
    Improvements and expansion

--- dothing.sh
+++ dothing.sh
@@ -1,3 +1,3 @@
-#the install, a basic outline
+#the install command, a very basic outline
 sudo apt full-upgrade && sudo apt install gh git pipx #&& git stuff
-pipx ensurepath && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && poetry -vvv install --all-groups --all-extras --compile && python3 main.py
+sudo pipx ensurepath -- global && pipx install poetry && pipx upgrade poetry && poetry completions bash >> ~/.bash_completion && eval $(poetry env activate) && poetry -vvv install --all-groups --all-extras --compile && poetry run python main.py 
