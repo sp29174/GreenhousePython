5f4d8a9bb69f5f3aaa10569a50ffa7c368f25ad7
ni34248 <ni34248@students.mcpasd.k12.wi.us>
2025-12-10 09:27:11 -0600

    Update lights.py
    fixes

--- src/lights.py
+++ src/lights.py
@@ -3,7 +3,7 @@ from datetime import timedelta, timezone, tzinfo
 from suntime import Sun, SunTimeException
 import RPi.GPIO as GPIO
 
-# Lighting GPIO
+# Lighting GPIO (GPIO pinouts will probably have to change when we get things working)
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(21, GPIO.OUT)
 
@@ -11,15 +11,16 @@ def lights(light_length):
   latitude = 43.0972
   longitude = -89.5043
 
-  mcpasd = datetime.datetime.now(timezone.utc) - timedelta(hours=5)
+  mcpasd = datetime.datetime.now() - timedelta(hours=6)
 
   sun = Sun(latitude, longitude)
 
   light_on = False
 
   # Get today's sunrise and sunset in CST
-  today_sr = sun.get_sunrise_time() + timedelta(hours=7)
-  today_ss = sun.get_sunset_time() + timedelta(hours=7)
+  today_sr = sun.get_sunrise_time() - timedelta(hours=6)
+  today_ss = sun.get_sunset_time() - timedelta(hours=6)
+  # unsure of use of this
   if today_sr > mcpasd:
     today_sr = today_sr - timedelta(days=1)
   if today_sr > today_ss:
@@ -27,13 +28,12 @@ def lights(light_length):
     
   today_suntime = today_ss - today_sr
 
-  light_on = today_suntime - today_suntime + timedelta(hours = light_length)
-
-  today_suntime = mcpasd - today_sr
-
-  if(mcpasd.time() > today_ss.time() and today_suntime < light_on):
-    light_on = True
-  else:
+  if(mcpasd > today_sr and mcpasd < today_ss):
     light_on = False
+    return light_on
+  else:
+    light_on = True
+    return light_on
   GPIO.output(21, True)
+
 
