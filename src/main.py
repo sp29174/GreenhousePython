5452dd5316c14c3b3cdeb68660aaad27da23fe5a
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-01-13 12:27:56 -0600

    Update main.py

--- src/main.py
+++ src/main.py
@@ -6,8 +6,10 @@ camera_nonsense = False
 if camera_nonsense:
 	from picamera2 import Picamera2
 	import cv2
-	import RPi.GPIO as GPIO
 	from PIL import Image, ImageTk
+else:
+	from nonsense import Picamera2, cv2, Image, ImageTk
+import RPi.GPIO as GPIO
 import tkinter as tk
 from tkinter import ttk
 import datetime
@@ -302,6 +304,7 @@ camera_cfg = picam2.create_still_configuration()
 theCamera.start()
 window.after(dt, lambda : repeater(dt,latitude,longitude))
 window.mainloop()
+
 
 
 
