44d115713a2196c6906e6baeed98d089362bc577
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-01-15 11:38:18 -0600

    Update nonsense.py

--- src/greenhousepython/nonsense.py
+++ src/greenhousepython/nonsense.py
@@ -17,3 +17,29 @@ class GPIO:
     pass
   def output(pin,thing):
     pass
+
+class busio:
+  def SPI(clock, MSIO, MOSI):
+    pass
+
+class digitalio:
+  def DigitalInOut(thing):
+    pass
+
+class board:
+  SCK = None
+  MISO = None
+  MOSI = None
+  CE0 = None
+
+class MCP:
+  P0 = None
+  P1 = None
+  P2 = None
+  def MCP3008(spi, cs):
+    return None
+
+class AnalogIn:
+  def __init__(self,mcp,pin):
+    self.value = None
+    self.voltage = None    
