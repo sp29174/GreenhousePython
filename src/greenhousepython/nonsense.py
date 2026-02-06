4700a6771b0ad9bb314b2a0f84744fc94d8c5eef
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-02-06 10:12:29 -0600

    Update nonsense.py
    Remove unused mocks

--- src/greenhousepython/nonsense.py
+++ src/greenhousepython/nonsense.py
@@ -20,28 +20,9 @@ class GPIO:
   def output(pin,thing):
     pass
 
-class busio:
-  def SPI(**kwargs):
-    pass
-
-class digitalio:
-  def DigitalInOut(thing):
-    pass
-
-class board:
-  SCK = None
-  MISO = None
-  MOSI = None
-  CE0 = None
-
 class MCP:
   P0 = None
   P1 = None
   P2 = None
   def MCP3008(spi, cs):
     return None
-
-class AnalogIn:
-  def __init__(self,mcp,pin):
-    self.value = 0
-    self.voltage = 0    
