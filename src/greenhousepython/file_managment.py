f9e5e108c7a3817e10f220d77e2cd57d19ad4628
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-03-12 11:30:32 -0500

    Update file_managment.py

--- src/greenhousepython/file_managment.py
+++ src/greenhousepython/file_managment.py
@@ -1,41 +1,43 @@
 import shelve
+import hashlib
 class wrapper_object:
 	def __init__():
-		attrs = shelve.open("cfg.txt", writeback = True)
-		if not attrs:
-			attrs["last_file_number"] = 0
-			attrs["camera_interval"] = 3600
-			attrs["interval"] = 1
-			attrs["file_name_prefix"] = "gi"
-			attrs["lights"] = 1
-			attrs["light_length0"] = 0.5
-			attrs["latitude"] = 43.0972
-			attrs["longitude"] = -89.5043
-			attrs["elevation"] = 355.0
-			attrs["light_pin0"] = 21
-			attrs["pump_pin"] = 15
-			attrs["water_pin0"] = 16
-			attrs["water_pin1"] = 17
-			attrs["water_pin2"] = 18
-			attrs["is_debug"] = True
-			attrs["is_recording"] = True
-			attrs["beds"] = 3
-			attrs["bed0"] = False
-			attrs["bed1"] = False
-			attrs["bed2"] = False
-			attrs["control_parameter0"] = 0.0
-			attrs["deadband0"] = 0.0
-			attrs["control_parameter1"] = 0.0
-			attrs["deadband1"] = 0.0
-			attrs["control_parameter2"] = 0.0
-			attrs["deadband2"] = 0.0
-			attrs.sync()
+		self.attrs = shelve.open("cfg.txt", writeback = True)
+		if not self.attrs:
+			self.attrs["last_file_number"] = 0
+			self.attrs["camera_interval"] = 3600
+			self.attrs["interval"] = 1
+			self.attrs["file_name_prefix"] = "gi"
+			self.attrs["lights"] = 1
+			self.attrs["light_length0"] = 0.5
+			self.attrs["latitude"] = 43.0972
+			self.attrs["longitude"] = -89.5043
+			self.attrs["elevation"] = 355.0
+			self.attrs["light_pin0"] = 21
+			self.attrs["pump_pin"] = 15
+			self.attrs["water_pin0"] = 16
+			self.attrs["water_pin1"] = 17
+			self.attrs["water_pin2"] = 18
+			self.attrs["is_debug"] = True
+			self.attrs["is_recording"] = True
+			self.attrs["beds"] = 3
+			self.attrs["bed0"] = False
+			self.attrs["bed1"] = False
+			self.attrs["bed2"] = False
+			self.attrs["control_parameter0"] = 0.0
+			self.attrs["deadband0"] = 0.0
+			self.attrs["control_parameter1"] = 0.0
+			self.attrs["deadband1"] = 0.0
+			self.attrs["control_parameter2"] = 0.0
+			self.attrs["deadband2"] = 0.0
+			self.attrs.sync()
 			#create default attrs
-	def get_attrs():
+	def data():
 		return attrs
-	def md5():
-		#find md5 hash of attrs
-		pass
+	def hash():
+		sha = hashlib.sha256()
+		sha.update(open("cfg.txt").read())
+		return sha.hexdigest()
 	def force_writeback(new_data):
 		attrs.close()
 		with open("cfg.txt",'w') as file:
