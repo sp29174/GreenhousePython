eab3b1ab9d71b26a9c61a96682ca8a6c404e17c6
The Guy With The Bad Username <75697942+yeetmanthecoolman@users.noreply.github.com>
2026-03-12 11:32:56 -0500

    Update server.py

--- src/greenhousepython/server.py
+++ src/greenhousepython/server.py
@@ -10,6 +10,8 @@ app = Typer(rich_markup_mode="rich")
 
 # Initialization ****************************************************************************************
 
+import greenhousepython.file_management as fm
+wrapper = fm.wrapper_object()
 try:
 	import cv2
 	x = cv2.VideoCapture(0)
@@ -21,19 +23,19 @@ try:
 	import mcp3008 as MCP
 	from mcp3008 import MCP3008
 except ImportError as e:
-	if attrs["is_debug"]:
+	if wrapper.attrs["is_debug"]:
 		print("WARNING: " + str(e))
 	from greenhousepython.nonsense import MCP, MCP3008
 try:
 	from greenhousepython.gpio_wrapper import gpio as g
 	GPIO = g()
 except Exception as e:
-	if attrs["is_debug"]:
+	if wrapper.attrs["is_debug"]:
 		print("WARNING: " + str(e))
 	try:
 		import RPi.GPIO as GPIO
 	except ImportError as e:
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		from greenhousepython.nonsense import GPIO
 import sys
@@ -45,48 +47,48 @@ from typing import Annotated
 # Postinitialization ****************************************************************************************
 
 times_off = []
-for n in range(attrs["lights"]):
+for n in range(wrapper.attrs["lights"]):
 	times_off.append(datetime.now(timezone.utc))
 # create the mcp object
 mcp = MCP3008.fixed([MCP.CH0, MCP.CH1, MCP.CH2, MCP.CH3, MCP.CH4, MCP.CH5, MCP.CH6, MCP.CH7])
 # setup other GPIO
 GPIO.setmode(GPIO.BCM)
-for x in range(attrs["lights"]):
-	GPIO.setup(attrs["light_pin" + str(x)], GPIO.OUT)
-for x in range(attrs["beds"]):
-	GPIO.setup(attrs["water_pin" + str(x)], GPIO.OUT)
-GPIO.setup(attrs["pump_pin"], GPIO.OUT)
+for x in range(wrapper.attrs["lights"]):
+	GPIO.setup(wrapper.attrs["light_pin" + str(x)], GPIO.OUT)
+for x in range(wrapper.attrs["beds"]):
+	GPIO.setup(wrapper.attrs["water_pin" + str(x)], GPIO.OUT)
+GPIO.setup(wrapper.attrs["pump_pin"], GPIO.OUT)
 the_camera = cv2.VideoCapture(0)
 
 # More helpers ***********************************************************************************
 def do_shutdown(*args,**kwargs):
 	global mcp
 	global the_camera
-	global attrs
-	if attrs["is_debug"]:
+	global wrapper.attrs
+	if wrapper.attrs["is_debug"]:
 		print("Shutting down...")
 	try:#we shouldn't let crashes prevent the program from closing, so these must all be wrapped with try.
 		mcp.close()#close down water control coms
 	except Exception as e:
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("Warning: we couldn't close down the water control communitcations.")
 	try:
 		the_camera.release()#turn off the damn camera
 	except Exception as e:
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("Warning: we couldn't close down the camera.")
 	try:
 		GPIO.cleanup()#knock the GPIO back to high-zed
 	except Exception as e:
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("Warning: we couldn't reset the GPIO.")
-	attrs.close()
+	wrapper.attrs.close()
 	sys.exit(0)#ensure the proghramme actually ends
 
 # Postpostinitialization ***********************************************************************************
@@ -127,33 +129,33 @@ def change_setting(key : Annotated[str, Argument(help="The exact name of the set
 	else:
 		print("Confusion noise: '" + key + "' is not actually a thing.")
 		return None
-	attrs[setting_to_change] = new_val
-	attrs.sync()
+	wrapper.attrs[setting_to_change] = new_val
+	wrapper.attrs.sync()
 
 #control pumps using hysteresis based on the values returned from the MCP
 @app.command(help="Force the system to run the automatic water control logic without starting the GUI.")
 def water():
-	global attrs
+	global wrapper.attrs
 	run_pump = False
 	try:#my crummy mock of mcp3008.MCP3008 can't make sense of line 118, so this eyesore is trapped in a try-catch.
-		for x in range(attrs["beds"]):
+		for x in range(wrapper.attrs["beds"]):
 			moisture = mcp(1)[x]#normalize values to one using mcp3008.MCP3008 (see https://github.com/luxedo/RPi_mcp3008/blob/master/mcp3008.py#L73)
-			if (not attrs["bed" + str(x)]) and (moisture < attrs["control_parameter" + str(x)] - (attrs["deadband" + str(x)]/2)):#this if-else is basically an inelegant hysteresis controller. On the other hand, we're being rewarded for not destroying the pump, not for elegantly destroying the pump.
-				GPIO.output(int(attrs["water_pin" + str(x)]), GPIO.HIGH)
-				attrs["bed" + str(x)] = "True"
-				attrs.sync()
-			elif attrs["bed" + str(x)] and (moisture > attrs["control_parameter" + str(x)] + (attrs["deadband" + str(x)]/2)):
-				GPIO.output(attrs["water_pin" + str(x)], GPIO.LOW)
-				attrs["bed" + str(x)] = False
-				attrs.sync()
-			if (attrs["bed" + str(x)] == "True"):
+			if (not wrapper.attrs["bed" + str(x)]) and (moisture < wrapper.attrs["control_parameter" + str(x)] - (wrapper.attrs["deadband" + str(x)]/2)):#this if-else is basically an inelegant hysteresis controller. On the other hand, we're being rewarded for not destroying the pump, not for elegantly destroying the pump.
+				GPIO.output(int(wrapper.attrs["water_pin" + str(x)]), GPIO.HIGH)
+				wrapper.attrs["bed" + str(x)] = "True"
+				wrapper.attrs.sync()
+			elif wrapper.attrs["bed" + str(x)] and (moisture > wrapper.attrs["control_parameter" + str(x)] + (wrapper.attrs["deadband" + str(x)]/2)):
+				GPIO.output(wrapper.attrs["water_pin" + str(x)], GPIO.LOW)
+				wrapper.attrs["bed" + str(x)] = False
+				wrapper.attrs.sync()
+			if (wrapper.attrs["bed" + str(x)] == "True"):
 				run_pump = True#If any bed is on, then run the pump.
 		if run_pump:
-			GPIO.output(int(attrs["pump_pin"]), GPIO.HIGH)
+			GPIO.output(int(wrapper.attrs["pump_pin"]), GPIO.HIGH)
 		else:
-			GPIO.output(int(attrs["pump_pin"]), GPIO.LOW)
+			GPIO.output(int(wrapper.attrs["pump_pin"]), GPIO.LOW)
 	except Exception as e:
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print("WARNING: " + str(e))
 		else:
 			print("We failed at water control. This is probably because we aren't connected to an MCP3008, which is reasonable. If it isn't reasonable, check the dependencies.")
@@ -169,55 +171,55 @@ def light():#This code is a disaster area. Essentially, here's the logic:
 	#A consequence of this is that if you adjust the light length at any reasonable hour, it will only update the next day.
 	#Another consequence of this is that if you run this code in Iceland or something where the sun won't rise on certain days of the year, this code will have to bodge the sun.
 	#There must be a better solution than this, but I couldn't find it. Shrug emoji.
-	global attrs
+	global wrapper.attrs
 	global times_off
-	observer = Observer(attrs["latitude"],attrs["longitude"],attrs["elevation"])
+	observer = Observer(wrapper.attrs["latitude"],wrapper.attrs["longitude"],wrapper.attrs["elevation"])
 	try:
 		the_sun = sun.daylight(observer)#The sun is a deadly laser	
 	except ValueError as e:#the sun doesn't rise/set today. We therefore need to fudge the sun. This means that, for our purposes, the sun rose at midnight and set a very long time ago.
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print(str(e))
 		the_sun = [datetime(datetime.year,datetime.month,datetime.day),datetime.min]
 	if (datetime.now(timezone.utc) >= the_sun[1]):
-		if attrs["is_debug"]:
+		if wrapper.attrs["is_debug"]:
 			print("We think that it's after sunset.")
-		for n in range(attrs["lights"]):
-			times_off[n] = the_sun[0] + timedelta(days=attrs["light_length" + str(n)])
-	elif attrs["is_debug"]:
+		for n in range(wrapper.attrs["lights"]):
+			times_off[n] = the_sun[0] + timedelta(days=wrapper.attrs["light_length" + str(n)])
+	elif wrapper.attrs["is_debug"]:
 		print("We think that it's before sunset.")
-	for n in range(attrs["lights"]):
+	for n in range(wrapper.attrs["lights"]):
 		light_on = False
 		if (datetime.now(timezone.utc) < times_off[n]):
 			light_on = True
-			if attrs["is_debug"]:
+			if wrapper.attrs["is_debug"]:
 				print("The light should be on.")
-		elif attrs["is_debug"]:
+		elif wrapper.attrs["is_debug"]:
 			print("The light should be off.")
 		if light_on:
-			GPIO.output(attrs["light_pin" + str(n)], GPIO.HIGH)
+			GPIO.output(wrapper.attrs["light_pin" + str(n)], GPIO.HIGH)
 		else:
-			GPIO.output(attrs["light_pin" + str(n)], GPIO.LOW)
+			GPIO.output(wrapper.attrs["light_pin" + str(n)], GPIO.LOW)
 
-#A command that captures a photograph, writes it to a file, and updates attrs accordingly.
+#A command that captures a photograph, writes it to a file, and updates wrapper.attrs accordingly.
 @app.command(help="Manually capture a photograph.")
 def camera_capture():#updated to not badly reimplement last_file_name
-	global attrs
+	global wrapper.attrs
 	global the_camera
 	ret, frame = the_camera.read()
 	if not ret:
-		if not attrs["is_debug"]:
+		if not wrapper.attrs["is_debug"]:
 			assert False#Fuck this!
 		else:
 			print("Warning: Image capture failed to complete.")
 	else:
-		cv2.imwrite(get_file_name(attrs["last_file_number"] + 1), frame)
-		attrs["last_file_number"] = attrs["last_file_number"] + 1
-		attrs.sync()
+		cv2.imwrite(get_file_name(wrapper.attrs["last_file_number"] + 1), frame)
+		wrapper.attrs["last_file_number"] = wrapper.attrs["last_file_number"] + 1
+		wrapper.attrs.sync()
 
 @app.command(help="Assemble the current collection of images into a mp4 video. This will:\n \n 1. Not include the placeholder image, ever. \n 2. Not recoginze images created with file numbers above last_file_number. \n 3. Pollute your shell with warnings if you have nonsequential image numbers. \n 4. Be unaware of the fact that manually captured images are not taken at regular intervals.")
 def create_video(output_video_path : Annotated[str, Argument(help="An exact file name, which must be writable, must not have stuff in it, and should not be irrational. If you run this command with sudo and pass /, then it will replace your system with a video. Don't.")], fps : Annotated[int, Option(help="The framerate that the video will be displayed at. Defaults to 24.")] = 24, size : Annotated[str, Option(help="An override to resize your images to a different resolution. This is needed internally for technical reasons, and we saw no reason not to expose it to the commandline. Defaults to your camera resolution.")] = None):
 	image_paths = []
-	for num in range(1,attrs["last_file_number"] + 1):
+	for num in range(1,wrapper.attrs["last_file_number"] + 1):
 		if cv2.imread(get_file_name(num)) is not None:
 			image_paths.append(get_file_name(num))
 		else:
@@ -242,18 +244,18 @@ def create_video(output_video_path : Annotated[str, Argument(help="An exact file
 def see_data():
 	global times_off
 	global mcp
-	global attrs
+	global wrapper.attrs
 	print(times_off)
 	print(mcp())
 	print(mcp(1))
-	keys = attrs.keys()#get all the settings
+	keys = wrapper.attrs.keys()#get all the settings
 	for key in keys:
-		print(key + ":" + str(attrs[key]))#assemble key and values into new format
+		print(key + ":" + str(wrapper.attrs[key]))#assemble key and values into new format
 
 #A quick little command that just starts the GUI.
 @app.command(help="Starts the main server thing.")
 def open_socket(port : Annotated[int, Argument(help="The port to open a socket on.")], reverseport : Annotated[int, Argument(help="The port to recieve data from.")]):
-	global attrs
+	global wrapper.attrs
 	s = socket.socket()
 	s.bind(('',port))
 	s.listen(1)
@@ -263,10 +265,11 @@ def open_socket(port : Annotated[int, Argument(help="The port to open a socket o
 	#socket logic goes here
 
 # Finalization and execution ****************************************************************************************
-if attrs["is_debug"]:
+if wrapper.attrs["is_debug"]:
 	print(__name__)
 if __name__ == "__main__":
 	app()
+
 
 
 
